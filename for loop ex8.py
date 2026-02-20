b712daefb432d4b0c58da7ea02cd154f6f8021bf
Chandru S <chandrusangar8@gmail.com>
2026-02-20 22:05:54 +0530

    Add files via upload

---
+++ for loop ex8.py
@@ -0,0 +1,8 @@
+for i in range(1,6):
+    print("the number is:",i,"and sqaure",i,"is:",i*i)
+
+
+a=int(input("enter how many terms:"))
+for i in range(1,a+1):
+    print("the number is:",i,"and sqaure",i,"is:",i*i)
+    
---
+++ for loop ex9.py
@@ -0,0 +1,7 @@
+for i in range(1,6):
+    print("the number is:",i,"and power4 of",i,"is:",i**4)
+
+a=int(input("enter number of terms:"))
+for i in range(1,a+1):
+    print("the number is:",i,"and power4 of",i,"is:",i**4)
+    
