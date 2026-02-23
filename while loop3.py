3e79d1663fea017eb0578f9bbcb5a75b58b9dc76
Chandru S <chandrusangar8@gmail.com>
2026-02-23 22:02:33 +0530

    Add files via upload

---
+++ while loop3.py
@@ -0,0 +1,9 @@
+i=int(input())
+while(i>0):
+    print(i,end=" ")
+    i=i-1
+
+i=10
+while(i<=200):
+    print(i,end=" ")
+    i=i+10
---
+++ while loop4.py
@@ -0,0 +1,7 @@
+i=int(input("enter the number:"))
+fact=1
+while(i>0):
+    fact=fact*i
+    i=i-1
+print(fact)
+    
