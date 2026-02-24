492e46cc998411e8735987beb2d40c6e6257f6b6
Chandru S <chandrusangar8@gmail.com>
2026-02-24 22:06:47 +0530

    Add files via upload

---
+++ python collections2.py
@@ -0,0 +1,10 @@
+a=[10,20,30,40,50]
+print(a)
+a.pop(1)
+print(a)
+a.pop()
+print(a)
+
+
+
+
---
+++ python collections3.py
@@ -0,0 +1,11 @@
+
+a=[1,2,3,4,5]
+print(a)
+a.insert(1,7)
+print(a)
+a.pop(3,4)
+print(a)
+a[2]=66
+print(a)
+print(a[2])
+
