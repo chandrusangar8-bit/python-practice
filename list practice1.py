edf5168b0410dad0154fd8300ea55ca3ad12f6bb
Chandru S <chandrusangar8@gmail.com>
2026-02-27 22:01:22 +0530

    Add files via upload

---
+++ list practice1.py
@@ -0,0 +1,11 @@
+
+marks=[]
+for i in range(5):
+    t=int(input("enter mark:"))
+    marks.append(t)
+    print(marks)
+   
+print("total marks:",sum(marks))
+print("highest mark:",max(marks))
+print("lowest mark:",min(marks))
+
---
+++ tuple practice1.py
@@ -0,0 +1,4 @@
+student=("chand",20,"IT")
+print(student[2])
+student[1]=21
+print(student)
