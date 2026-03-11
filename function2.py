9cb0cbf74ec1fa8bdd036ec1fc0a2345617649dc
Chandru S <chandrusangar8@gmail.com>
2026-03-11 21:57:18 +0530

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,15 @@
+def add():
+    print("addition")
+    a=int(input("a:"))
+    b=int(input("b:"))
+    print("a+b:",a+b)
+
+
+def sub():
+    print("subtraction")
+    a=int(input("a:"))
+    b=int(input("b:"))
+    print("a-b:",a-b)
+
+add()
+sub()
---
+++ function3.py
@@ -0,0 +1,16 @@
+def mul():
+    print("multiplication")
+    a=int(input("a:"))
+    b=int(input("b:"))
+    print("a*b:",a*b)
+mul()
+
+def div():
+    print("division")
+    a=int(input("a:"))
+    b=int(input("b:"))
+    print("a/b:",a/b)
+div()
+    
+
+    
